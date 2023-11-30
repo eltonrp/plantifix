2d2492244a3930c458c9efd5610d503a4bf467e8
eltonrp <eltonrpo@gmail.com>
2023-11-29 23:37:04 -0300

    efeitos no btn-menu e correcoes

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const btnMenu = document.getElementById('btn-menu')
+  const mobileMenu = document.getElementById('mobile-menu')
+  const span1 = document.getElementById('span1')
+  const span2 = document.getElementById('span2')
+  const span3 = document.getElementById('span3')
+  // btnMenu.addEventListener('click', () => {
+  //     mobileMenu.classList.toggle('hidden')
+  // })
+  function onToggleMenu(e) {
+    span1.classList.toggle('origin-center')
+    span1.classList.toggle('rotate-45')
+    span1.classList.toggle('translate-y-[7px]')
+    span2.classList.toggle('hidden')
+    span2.classList.toggle('origin-center')
+    span3.classList.toggle('-rotate-45')
+    btnMenu.classList.toggle('border')
+    mobileMenu.classList.toggle('top-[100%]')
+  }    
